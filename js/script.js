79d05e94a2f056f88886a0959be539bcb5a5015c
Nikita <needsthxxx@gmail.com>
2020-04-06 16:43:21 +0300

    added a few minimalism

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
-let simpleRunTime = document.querySelector('.simple-time'),
-    eachRunSplit = document.querySelector('.each-split'),
-    tableOfSplits = document.getElementsByClassName('splits-table')[0],
-    startPauseBtn = document.getElementById('toggle-btn'),
-    splitBtn = document.getElementById('split-btn'),
-    resetBtn = document.getElementById('reset-btn'),
-    splitTime = { 
+let simpleRunTime = document.querySelector(".simple-time"),
+    eachRunSplit = document.querySelector(".each-split"),
+    tableOfSplits = document.getElementsByClassName("splits-table")[0],
+    startPauseBtn = document.getElementById("toggle-btn"),
+    splitBtn = document.getElementById("split-btn"),
+    resetBtn = document.getElementById("reset-btn"),
+    splitTime = {
         millis: 0,
         sec: 0,
-        minutes: 0
+        minutes: 0,
     },
-    globalTime = { 
+    globalTime = {
         millis: 0,
         sec: 0,
-        minutes: 0
+        minutes: 0,
     },
-    globalId, splitId,
+    globalId,
+    splitId,
     counter = 0;
 
 function resetTimeVar() {
@@ -25,120 +26,122 @@ function resetTimeVar() {
         globalTime[key1] = 0;
     }
     counter = 0;
-    startPauseBtn.classList.remove('pause');
-    startPauseBtn.classList.add('start');
-    startPauseBtn.innerHTML = 'Start';
-    resetBtn.classList.add('display-none-class');
-    splitBtn.classList.add('display-none-class');
+    startPauseBtn.classList.remove("pause");
+    startPauseBtn.classList.add("start");
+    startPauseBtn.innerHTML = "Start";
+    resetBtn.classList.add("display-none-class");
+    splitBtn.classList.add("display-none-class");
 
-    let clildElements = tableOfSplits.getElementsByClassName('each-split');
+    let clildElements = tableOfSplits.getElementsByClassName("each-split");
     do {
         for (let key of clildElements) {
             tableOfSplits.removeChild(key);
         }
-    } while(clildElements.length != 0);
+    } while (clildElements.length != 0);
     clearTimeout(globalId);
     clearTimeout(splitId);
-    eachRunSplit.innerHTML = '00:00:00';
-    simpleRunTime.innerHTML = '00:00:00';
+    eachRunSplit.innerHTML = "00:00:00";
+    simpleRunTime.innerHTML = "00:00:00";
 }
 resetTimeVar();
 
-function timerClobalStart (){
+function timerClobalStart() {
     globalTime.millis++;
-    if(globalTime.millis >= 100) {
+    if (globalTime.millis >= 100) {
         globalTime.sec++;
         globalTime.millis -= 100;
     }
 
-    if(globalTime.sec >= 60) {
+    if (globalTime.sec >= 60) {
         globalTime.minutes++;
         globalTime.sec -= 60;
     }
 
-    if(globalTime.millis < 10) {
-        globalTime.millis = '0' + globalTime.millis;
-    } else globalTime.millis += '';
-    
-    if(globalTime.sec.toString().length <= 1) globalTime.sec = '0' + globalTime.sec;
-    else globalTime.sec += '';   
-    
-    if(globalTime.minutes.toString().length <= 1) globalTime.minutes = '0' + globalTime.minutes;
-    else globalTime.minutes += '';
+    if (globalTime.millis < 10) {
+        globalTime.millis = "0" + globalTime.millis;
+    } else globalTime.millis += "";
+
+    if (globalTime.sec.toString().length <= 1)
+        globalTime.sec = "0" + globalTime.sec;
+    else globalTime.sec += "";
+
+    if (globalTime.minutes.toString().length <= 1)
+        globalTime.minutes = "0" + globalTime.minutes;
+    else globalTime.minutes += "";
     simpleRunTime.innerHTML = `${globalTime.minutes}:${globalTime.sec}:${globalTime.millis}`;
     globalId = setTimeout(timerClobalStart, 9);
 }
 
-function timerSplitStart(){
+function timerSplitStart() {
     splitTime.millis++;
-    if(splitTime.millis >= 100) {
+    if (splitTime.millis >= 100) {
         splitTime.sec++;
         splitTime.millis -= 100;
     }
 
-    if(splitTime.sec >= 60) {
+    if (splitTime.sec >= 60) {
         splitTime.minutes++;
         splitTime.sec -= 60;
     }
 
-    if(splitTime.millis < 10) {
-        splitTime.millis = '0' + splitTime.millis;
-    } else splitTime.millis += '';
-    
-    if(splitTime.sec.toString().length <= 1) splitTime.sec = '0' + splitTime.sec;
-    else splitTime.sec += '';   
-    
-    if(splitTime.minutes.toString().length <= 1) splitTime.minutes = '0' + splitTime.minutes;
-    else splitTime.minutes += '';
+    if (splitTime.millis < 10) {
+        splitTime.millis = "0" + splitTime.millis;
+    } else splitTime.millis += "";
+
+    if (splitTime.sec.toString().length <= 1)
+        splitTime.sec = "0" + splitTime.sec;
+    else splitTime.sec += "";
+
+    if (splitTime.minutes.toString().length <= 1)
+        splitTime.minutes = "0" + splitTime.minutes;
+    else splitTime.minutes += "";
     eachRunSplit.innerHTML = `${splitTime.minutes}:${splitTime.sec}:${splitTime.millis}`;
     splitId = setTimeout(timerSplitStart, 9);
 }
 
-startPauseBtn.addEventListener('click', function(e) {
-    
-    if(e.target.classList.contains('start')) {
-        e.target.classList.remove('start');
-        e.target.classList.add('pause');
-        splitBtn.classList.remove('display-none-class');
-        e.target.innerHTML = 'Pause';
+startPauseBtn.addEventListener("click", ({ target }) => {
+    if (target.classList.contains("start")) {
+        target.classList.remove("start");
+        target.classList.add("pause");
+        splitBtn.classList.remove("display-none-class");
+        target.innerHTML = "Pause";
 
         timerClobalStart();
         timerSplitStart();
     } else {
-
         clearTimeout(globalId);
         clearTimeout(splitId);
 
-        e.target.classList.remove('pause');
-        e.target.classList.add('start');
-        e.target.innerHTML = 'Start';
-        resetBtn.classList.remove('display-none-class');
+        target.classList.remove("pause");
+        target.classList.add("start");
+        target.innerHTML = "Start";
+        resetBtn.classList.remove("display-none-class");
     }
 });
 
-splitBtn.addEventListener('click', function(e) {
+splitBtn.addEventListener("click", function (e) {
     SplitViewer(eachRunSplit.innerHTML, counter++);
     for (const key in splitTime) {
         splitTime[key] = 0;
-        eachRunSplit.innerHTML = '00:00:00';
+        eachRunSplit.innerHTML = "00:00:00";
     }
 });
 
-resetBtn.addEventListener('click', resetTimeVar);
+resetBtn.addEventListener("click", resetTimeVar);
 
 // функция конструктор для отображения кругов в колонке справа
 function SplitViewer(date, index) {
-    let eachSplit = document.createElement('div'),
-        splitsTable = document.querySelector('.splits-table'),
-        spanTime = document.createElement('span');
-    
-    eachSplit.classList.add('each-split');
+    let eachSplit = document.createElement("div"),
+        splitsTable = document.querySelector(".splits-table"),
+        spanTime = document.createElement("span");
 
-    if(index < 10) {
+    eachSplit.classList.add("each-split");
+
+    if (index < 10) {
         eachSplit.innerHTML = `#${index + 1} `;
         spanTime.innerHTML = date;
         eachSplit.appendChild(spanTime);
     } else eachSplit.innerHTML = `Please stop!`;
-    
+
     splitsTable.appendChild(eachSplit);
 }
